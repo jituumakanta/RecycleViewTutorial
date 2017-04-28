c1e38acbbb91c87f6af1195d9ddbb222549bd5c1
jituumakanta <jitu.umakanta@gmail.com>
2017-04-28 17:25:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jitu/recycleviewtutorial/RecyclerViewClickListener.java
@@ -0,0 +1,12 @@
+package com.example.jitu.recycleviewtutorial;
+
+import android.view.View;
+
+/**
+ * Created by Lucky on 2/18/2016.
+ */
+public interface RecyclerViewClickListener {
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+}
